42c9e85eca89e80be3ef3bb29dfae5625619fb1c
Praveen <saipraveenneelisetty27@gmail.com>
2024-05-12 22:50:55 +0530

    terraform

--- expense-practice/route53.tf
+++ expense-practice/route53.tf
@@ -1,8 +1,9 @@
-resource aws_route53_record "Expense" {
+resource aws_route53_record "expense" {
     count = length(var.instance_name)
     type = "A"
     zone_id = var.zone_id
     ttl = 1
     name = var.instance_name[count.index] == "frontend" ? var.domain_name : "${var.instance_name[count.index]}.${var.domain_name}"
-    records = var.instance_name[count.index] == "frontend" ? var.aws_route53_record.Expense.public_ip : "${var.instance_name}.${var.aws_route53_record.Expense.private_ip}"
+    records = var.instance_name[count.index] == "frontend" ? [aws_instance.AWS_Instances[count.index].public_ip] : [aws_instance.AWS_Instances[count.index].private_ip]
+    allow_overwrite = true
 }
